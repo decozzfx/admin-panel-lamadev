7a7226e9042582af085ab4e321c07cb976412e88
decoz <m.f.r.ozil@gmail.com>
2022-04-12 09:06:18 +0700

    build table component

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -30,12 +30,19 @@ const TableComp = () => {
           {rows.map((row) => (
             <TableRow key={row.id}>
               <TableCell className='tableCell'>{row.id}</TableCell>
-              <TableCell className='tableCell'>{row.product}</TableCell>
+              <TableCell className='tableCell'>
+					  <div className="cellWrapper">
+                		<img src={row.img} alt='' className='image' />
+							 {row.product}
+					  </div>
+                </TableCell>
               <TableCell className='tableCell'>{row.customer}</TableCell>
               <TableCell className='tableCell'>{row.date}</TableCell>
               <TableCell className='tableCell'>{row.amount}</TableCell>
               <TableCell className='tableCell'>{row.method}</TableCell>
-              <TableCell className='tableCell'>{row.status}</TableCell>
+              <TableCell className='tableCell'>
+					  <span className={`status ${row.status}`}>{row.status}</span>
+				  </TableCell>
             </TableRow>
           ))}
         </TableBody>
